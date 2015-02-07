f4527186bdd31180ea7069f49e35657dee218cd2
Yegor Kolesnikov <kelegorkel@gmail.com>
2015-02-07 02:58:24 +0200

    almost ready addGuestCtrl

--- src/components/addguest/addguest-controller.js
+++ src/components/addguest/addguest-controller.js
@@ -1,3 +1,17 @@
-/**
- * Created by yegor on 06.02.15.
- */
+'use strict';
+
+angular.module('clubguests')
+  .controller('addGuestCtrl', ['$scope','idGen', function ($scope, idGen) {
+
+    $scope.add = function (newName) {
+
+      var guest = {
+        id: idGen.getId(),
+        name: newName,
+        inHall: false
+      };
+
+      $scope.guests.push(guest);
+    };
+
+  }]);
